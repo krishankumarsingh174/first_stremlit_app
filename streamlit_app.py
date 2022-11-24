0150af024e3693811aa3ce5acb444cd2184e7d84
krishankumarsingh174 <118990806+krishankumarsingh174@users.noreply.github.com>
2022-11-25 00:51:52 +0100

    Update streamlit_app.py
    added some text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
 
 streamlit.title("This is the First page")
+streamlit.header("This is the header")
+streamlit.text("This is where the text goes")
+streamlit.text("This is where the text goes 1")
+streamlit.text("This is where the text goes 2")
 
